ef0494f70baa5e484133b906b6294b34c66d2236
JaeYoung0 <hjy8697@gmail.com>
2021-09-21 22:00:50 +0900

    style: cloudLayout animation

--- src/layouts/CloudLayout/CloudLayout.tsx
+++ src/layouts/CloudLayout/CloudLayout.tsx
@@ -1,15 +1,27 @@
 import React from "react";
 import styled from "@emotion/styled";
 import { MEDIA_QUERY_ARR } from "@styles/MediaQuery";
+import { keyframes } from "@emotion/react";
+
+const 흘러라구름아 = keyframes`
+  from {
+    background-position: 0 0;
+  }
+  to {
+    background-position: -1000rem 0;
+  }
+`;
 
 const Background = styled.div`
   position: relative;
   width: 100vw;
   height: 100vh;
   background-image: url("https://images.unsplash.com/photo-1498496294664-d9372eb521f3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80)");
-  background-position: center;
-  background-repeat: no-repeat;
+
+  background-repeat: repeat;
   background-size: cover;
+
+  animation: ${흘러라구름아} 500s linear infinite;
 `;
 
 const FilterWrapper = styled.div`

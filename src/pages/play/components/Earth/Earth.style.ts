c745383eeec31685e2673f2679acf51b8e7a3572
JaeYoung0 <hjy8697@gmail.com>
2022-02-18 18:09:44 +0900

    feat: Stars, useFrame

--- src/pages/play/components/Earth/Earth.style.ts
+++ src/pages/play/components/Earth/Earth.style.ts
@@ -3,5 +3,5 @@ import styled from "@emotion/styled";
 export const CanvasContainer = styled.div`
   width: 100%;
   height: 100vh;
-  background: #efefef;
+  background: #000000;
 `;
--- src/pages/play/components/Earth/Earth.tsx
+++ src/pages/play/components/Earth/Earth.tsx
@@ -1,14 +1,14 @@
-import { Suspense } from "react";
+import { Suspense, useRef } from "react";
 import * as S from "./Earth.style";
-import { Canvas, useLoader } from "@react-three/fiber";
+import { Canvas, MeshProps, useFrame, useLoader } from "@react-three/fiber";
 
 import EarthLightsMap from "./assets/textures/10k_earthlights.jpg";
 import EarthCloudsMap from "./assets/textures/earthcloudmap.jpg";
 import EarthSpecularMap from "./assets/textures/earthspec1k.jpg";
 import * as THREE from "three";
-import { TextureLoader } from "three";
+import { Euler, TextureLoader } from "three";
 
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, Stars } from "@react-three/drei";
 
 /**
  * 공간 - Scene
@@ -24,10 +24,20 @@ function EarthGlobe() {
   ]);
   console.log(lightsMap);
 
+  const earthRef = useRef<MeshProps>();
+  const cloudsRef = useRef<MeshProps>();
+
+  useFrame(({ clock }) => {
+    const elapsedTime = clock.getElapsedTime();
+    earthRef.current?.setRotationFromEuler?.(new Euler(0, elapsedTime / 6));
+    cloudsRef.current?.setRotationFromEuler?.(new Euler(0, elapsedTime / 6));
+  });
+
   return (
     <>
       <ambientLight intensity={0.5} />
-      <mesh>
+      <Stars radius={300} depth={100} count={5000} saturation={50} factor={5} />
+      <mesh ref={cloudsRef}>
         <sphereGeometry args={[1.005, 32, 32]} />
         <meshPhongMaterial
           map={cloudsMap}
@@ -37,13 +47,18 @@ function EarthGlobe() {
           side={THREE.DoubleSide}
         />
       </mesh>
-      <mesh>
+      <mesh ref={earthRef}>
         <sphereGeometry args={[1, 32, 32]} />
         {/* meshPhongMaterial: 반사 하이라이트가 있는 광택있는 표면 재질을 표현, 빛이 없으면 그냥 검은색으로 보임 */}
-        <meshPhongMaterial color="red" specularMap={specularMap} />
+        <meshPhongMaterial specularMap={specularMap} />
 
         {/* A standard physically based material, using Metallic-Roughness workflow. */}
-        <meshStandardMaterial map={lightsMap} normalMap={cloudsMap} />
+        <meshStandardMaterial
+          map={lightsMap}
+          normalMap={cloudsMap}
+          // metalness={0.3}
+          // roughness={0.5}
+        />
 
         {/* Orbit controls allow the camera to orbit around a target.
          */}
@@ -52,8 +67,8 @@ function EarthGlobe() {
           enablePan
           enableRotate
           zoomSpeed={0.6}
-          panSpeed={0.5}
-          rotateSpeed={0.4}
+          panSpeed={0.6}
+          rotateSpeed={1}
         />
       </mesh>
     </>

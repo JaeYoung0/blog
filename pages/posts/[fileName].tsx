b5274d264d469a052d33876a14d8aff3770a81c7
허재영 <jaeyoung.heo@qualson.com>
2022-07-01 01:43:41 +0900

    fix: mdx

--- pages/posts/[fileName].tsx
+++ pages/posts/[fileName].tsx
@@ -2,37 +2,55 @@ import MarkdownRenderer from "@components/MarkdownRenderer";
 import { GetStaticPaths, GetStaticProps } from "next";
 import DefaultLayout from "@layouts/DefaultLayout";
 import { getAllPosts, getPostData } from "@pages/posts/helper";
+import { serialize } from "next-mdx-remote/serialize";
+import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote";
+import rehypePrism from "rehype-prism-plus";
+import "github-markdown-css/github-markdown.css";
+import { Wrapper } from "@components/MarkdownRenderer/MarkdownRenderer.style";
+import Input from "@components/Input";
 
-interface Props {
+interface PostPageProps {
   meta: {
     date: string | number;
     title: string;
   };
 
   content: string;
+  mdxSource: MDXRemoteSerializeResult<Record<string, unknown>>;
 }
 
-export default function Post({ meta, content }: Props) {
+const components = { Input };
+
+function PostPage({ meta, content, mdxSource }: PostPageProps) {
   return (
     <DefaultLayout backgroud="#0d1117">
-      <MarkdownRenderer markdownContent={content} />
+      {/* <MarkdownRenderer markdownContent={content} /> */}
+      <Wrapper className="markdown-body">
+        <MDXRemote {...mdxSource} components={components} lazy />
+      </Wrapper>
     </DefaultLayout>
   );
 }
 
-export const getStaticProps: GetStaticProps<
-  any,
-  { fileName: string },
-  any
-> = async ({ params }) => {
+const getStaticProps: GetStaticProps<any, { fileName: string }, any> = async ({
+  params,
+}) => {
   const post = getPostData(params!.fileName);
+  const mdxSource = await serialize(post.content, {
+    mdxOptions: {
+      rehypePlugins: [rehypePrism],
+    },
+  });
 
   return {
-    props: post,
+    props: {
+      post,
+      mdxSource,
+    },
   };
 };
 
-export const getStaticPaths: GetStaticPaths = async () => {
+const getStaticPaths: GetStaticPaths = async () => {
   const posts = await getAllPosts();
 
   return {
@@ -46,3 +64,6 @@ export const getStaticPaths: GetStaticPaths = async () => {
     fallback: true,
   };
 };
+
+export { getStaticProps, getStaticPaths };
+export default PostPage;
